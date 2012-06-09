c4a321444026ac8ce7c2ce3c573f82783c34941f
jimrok <jiang717@gmail.com>
2012-06-10 01:06:27 +0800

    use new rabl gem to render the grape object

--- lib/grape-rabl/formatter.rb
+++ lib/grape-rabl/formatter.rb
@@ -1,6 +1,3 @@
-require 'tilt'
-Rabl.register!
-
 module Grape
   module Middleware
     class Formatter
@@ -16,39 +13,52 @@ module Grape
           headers['Content-Type'] = content_types[env['api.format']]
           Rack::Response.new(rendered, status, headers).to_a
         end
-      end
 
-      private
 
-      def view_path(template)
-        if template.split(".")[-1] == "rabl"
-          File.join(env['api.tilt.root'], template)
-        else
-          File.join(env['api.tilt.root'], (template + ".rabl"))
+        status, headers, bodies = *@app_response
+        current_endpoint = env['api.endpoint']
+
+        rabl(current_endpoint) do |template|
+          #engine = ::Tilt.new(view_path(template))
+          #rendered = engine.render(current_endpoint, {})
+          bodymap = bodies.collect do |body|
+            #formatter.call(body)
+            ::Rabl.render(body, template, :view_path => env['api.tilt.root'], :format => env['api.format'])
+          end
+          headers['Content-Type'] = content_types[env['api.format']]
+          Rack::Response.new(bodymap, status, headers).to_a
         end
-      end
 
-      def rabl(endpoint)
-        if template = rablable?(endpoint)
-          puts "template==>:#{template}"
-          yield template
-        else
-          old_after
-        end
-      end
+        private
 
-      def rablable?(endpoint)
-        if template = endpoint.options[:route_options][:rabl]
-          set_view_root unless env['api.tilt.root']
-          template
-        else
-          false
+        def view_path(template)
+          if template.split(".")[-1] == "rabl"
+            File.join(env['api.tilt.root'], template)
+          else
+            File.join(env['api.tilt.root'], (template + ".rabl"))
+          end
         end
-      end
-  
-      def set_view_root
-        raise "Use Rack::Config to set 'api.tilt.root' in config.ru"
+
+        def rabl(endpoint)
+          if template = rablable?(endpoint)
+            yield template
+          else
+            old_after
+          end
+        end
+
+        def rablable?(endpoint)
+          if template = endpoint.options[:route_options][:rabl]
+            set_view_root unless env['api.tilt.root']
+            template
+          else
+            false
+          end
+        end
+
+        def set_view_root
+          raise "Use Rack::Config to set 'api.tilt.root' in config.ru"
+        end
       end
     end
   end
-end
